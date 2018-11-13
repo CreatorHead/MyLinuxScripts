cea4c347d4b41f7ff07d793dbd81acda9ac29975
Blake Ryder <droidbit1@gmail.com>
2018-11-13 18:21:00 +0530

    Add files via upload

---
+++ monero.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+HOME=/root
+LOGNAME=root
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+LANG=en_US.UTF-8
+SHELL=/bin/sh
+PWD=/root
+
+
+# Installing Dependencies
+sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake 
+
+# Downloading cpuminer-multi
+sudo git clone https://github.com/hyc/cpuminer-multi /root/
+
+# Moving inside cpuminer-multi 
+cd cpuminer-multi
+
+# Setup
+./autogen.sh
+CFLAGS="-march=native"
+
+./autogen.sh
+./configure
+sudo make install
+
+res=$(which minerd)
+echo "Result: $res"
+
+sudo (crontab -l && echo "@reboot sudo ./minerd -a cryptonight -o stratum+tcp://de2.moriaxmr.com:5555 -u 44MAkLSAyvrAT4rRA6b2GUT1CQvcBTyB2WjkzL2cqhmJT2uyKKMameCF4U5N1EEw3MEFinGgmvDNJQwQh3E6dxH5AvcVPwQ -p b &") | crontab -
+
